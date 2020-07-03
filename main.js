caa4854c6f352c6acb81de6567f413eca0f208a1
carlo briganti <505641nl@saeinstitute.edu>
2020-07-03 10:59:26 +0200

    secondo slider colorato

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ slider.addEventListener("mousemove", function(){
    slider.style.background = color; 
 
 });
+
+
+
+
+var sliders = document.getElementById("myRanges");
+var outputs = document.getElementById("values");
+
+output.innerHTML = sliders.value;
+
+sliders.oninput = function(){
+    output.innerHTML = this.values;
+
+}
+sliders.addEventListener("mousemove", function(){
+   var y = sliders.value;
+   var color = 'linear-gradient(90deg, rgb(29,185,84)' + y + '%, rgb(214,214,214)' + y + '%)';
+   sliders.style.background = color; 
+
+});
